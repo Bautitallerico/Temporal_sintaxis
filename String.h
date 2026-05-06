754f5364a33d3ccaa6955b8324983f4598cc1d5f
Alejandro Aguilar Zabala <aaguilarzabala@frba.utn.edu.ar>
2026-05-06 14:48:19 -0300

    Update String.h
    Agregado

--- String.h
+++ String.h
@@ -7,3 +7,15 @@
 int IsEmpty(const char *str);
 
 #endif
+
+//Propuesta
+
+#ifndef STRING_H
+#define STRING_H
+
+int IsEmpty(const char *str);
+
+// Calcula la longitud de la cadena de forma recursiva
+int GetLength(const char *str);
+
+#endif
