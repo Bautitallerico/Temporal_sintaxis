4d6898ec2a664b86bbc38faf7444bc4adea9b887
Bautitallerico <btallerico@frba.utn.edu.ar>
2026-05-09 20:36:48 -0300

    isEmpty Bool

--- String.c
+++ String.c
@@ -13,76 +13,83 @@ int IsEmpty(const char *str)
     // Si el puntero es NULL o el primer carácter es el terminador nulo '\0'
     if (str == NULL || str[0] == '\0')
     {
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
-//Propuesta
+// Propuesta
 
+// Implementación Recursiva:
 
-
-   //Implementación Recursiva: 
-
-int GetLength(const char *str) {
-    if (str[0] == '\0') {
+int GetLength(const char *str)
+{
+    if (str[0] == '\0')
+    {
         return 0;
     }
     return 1 + GetLength(str + 1);
 }
 
-//funcion AreEqual
-int AreEqual (const char *str1, const char *str2)
+// funcion AreEqual
+int AreEqual(const char *str1, const char *str2)
 {
     int i;
-    for (i = 0; str1[i] != '\0' && str2[i] != '\0' ; i++)
+    for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++)
     {
         if (str1[i] != str2[i])
         {
             return 0;
         }
     }
-    if(str1[i] == str2[i]){
+    if (str1[i] == str2[i])
+    {
         return 1;
     }
     return 0;
 }
 
-//funcion areDecimalDigits
-// Precondición: 'str' es un puntero válido a una cadena terminada en '\0'.
-// Poscondición: Retorna true si todos los caracteres son dígitos. Si la cadena está vacía, debés decidir si retorna false (recomendado) o true.
-bool AreDecimalDigits(const char *str){
-    if(*str =='\0') {
-        return false ; 
-    }
-for (const char *p = str; *p != '\0'; p++)
+// funcion areDecimalDigits
+//  Precondición: 'str' es un puntero válido a una cadena terminada en '\0'.
+//  Poscondición: Retorna true si todos los caracteres son dígitos. Si la cadena está vacía, debés decidir si retorna false (recomendado) o true.
+bool AreDecimalDigits(const char *str)
 {
-    if(*p < '0' || *p > '9'){
+    if (*str == '\0')
+    {
         return false;
     }
-}
-return true;  
+    for (const char *p = str; *p != '\0'; p++)
+    {
+        if (*p < '0' || *p > '9')
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-//funcion Contains
-//precondiciones:str es un puntero a un bloque de memoria válido que contiene una cadena terminada en \0.
-//postcondiciones:Retorna true si el carácter charComp pertenece a la cadena apuntada por str; en caso contrario, retorna false.
+// funcion Contains
+// precondiciones:str es un puntero a un bloque de memoria válido que contiene una cadena terminada en \0.
+// postcondiciones:Retorna true si el carácter charComp pertenece a la cadena apuntada por str; en caso contrario, retorna false.
 
-bool Contains(char charComp, const char *str){ 
-    for(const char *p = str; *p != '\0';p++){
+bool Contains(char charComp, const char *str)
+{
+    for (const char *p = str; *p != '\0'; p++)
+    {
         if (*p == charComp)
         {
             return true;
-        }        
+        }
     }
     return false;
 }
 
-//funcion ToInteger
-//precondicion: str apunta a una cadena válida terminada en '\0' que representa un número entero en base 10 (asume que la cadena es correcta según consigna).
-//postcondicion: Devuelve el valor numérico entero equivalente a la cadena
+// funcion ToInteger
+// precondicion: str apunta a una cadena válida terminada en '\0' que representa un número entero en base 10 (asume que la cadena es correcta según consigna).
+// postcondicion: Devuelve el valor numérico entero equivalente a la cadena
 
-int ToInteger (const char *str){
+int ToInteger(const char *str)
+{
     int resultado = 0;
     int signo = 1;
 
@@ -91,10 +98,10 @@ int ToInteger (const char *str){
         signo = -1;
         str++;
     }
-    
+
     for (const char *p = str; *p != '\0'; p++)
     {
-       resultado = resultado * 10 + (*p - '0');
+        resultado = resultado * 10 + (*p - '0');
     }
     return resultado * signo;
 }
@@ -109,4 +116,3 @@ Esto funciona pq en ASCII el caracter '0' = 48 y (por ejemplo) el '3' = 51
 --> ()'3'-'0') == (51-48 = 3)
 Es por eso que hacemos resultado = resultado * 10 + (*p - '0')
 */
-
--- String.h
+++ String.h
@@ -3,22 +3,22 @@
 
 #include <stdbool.h>
 
-//verifica si una cadena está vacia
+// verifica si una cadena está vacia
 int IsEmpty(const char *str);
 
 // Calcula la longitud de la cadena de forma recursiva
 int GetLength(const char *str);
 
-//Informa si la cadena es igual
+// Informa si la cadena es igual
 int AreEqual(const char *str1, const char *str2);
 
-//indica si algun elemento no es decimal
+// indica si algun elemento no es decimal
 bool AreDecimalDigits(const char *str);
 
-//indica si una cadena dada tiene un caracter dado.
-bool Contains(char charComp, const char *str):
+// indica si una cadena dada tiene un caracter dado.
+bool Contains(char charComp, const char *str);
 
-//recibe una cadena de numeros y lo transforma en un entero
-int ToInteger (const char *str)
+// recibe una cadena de numeros y lo transforma en un entero
+int ToInteger(const char *str);
 
 #endif
