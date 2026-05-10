dd35494be9cf0a75de21270b16ef34ae979eedd4
thiago Radita <tradita@frba.utn.edu.ar>
2026-05-10 16:52:20 -0300

    suma.c(solo funciona en base 10)

--- suma.c
+++ suma.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "String.h"
-
+/*solo funciona en base 10*/
 int main(int argc, char *argv[])
 {
     int suma = 0;
 
-    for (char **p = argv + 1; p < argv + argc; p++)
+    for (char **p = argv + 1; *p != NULL; p++)
     {
         suma += ToInteger(*p);
     }
