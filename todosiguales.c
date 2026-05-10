ec91675b4bf8c0664291e1cdc1699ec6733f6fcc
Joaquín Alegría <joalegria@frba.utn.edu.ar>
2026-05-10 18:01:37 -0300

    Update todosiguales.c

--- todosiguales.c
+++ todosiguales.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include"String.h"
+
+int main (int argc, char *argv[]){
+
+    if(argc<=2){
+        printf("1\n");
+        return 0;
+    }
+
+    char *firstArgu = *(argv + 1);
+
+    for(char **p = argv + 2; *p != NULL; p++){
+        if(AreEqual(firstArgu, *p)==false){
+            printf("0\n");
+            return 0;
+        }
+    }
+
+    printf("1\n");
+    return 0;
+}
 
