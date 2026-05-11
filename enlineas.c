b77e2ccfd1da727990260cd9b37ccf445e21647b
Joaquín Alegría <joalegria@frba.utn.edu.ar>
2026-05-11 10:45:22 -0300

    Create enlineas.c

---
+++ enlineas.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"String.h"
+
+int main(int argc, char  *argv[]){
+
+    for(char **p = argv + 1; *p != NULL; p++){
+        printf("%s\n", *p);
+    }
+
+    return 0;
+}
