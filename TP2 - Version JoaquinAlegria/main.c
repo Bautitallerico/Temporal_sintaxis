c765eaee0cc70d25734aea51b4f62b7664fda628
agusfranco5 <agustinafranco516@gmail.com>
2026-09-13 22:29:29 -0300

    Cambio switch por arreglo de cadenas en main.c

--- TP2 - Version JoaquinAlegria/main.c
+++ TP2 - Version JoaquinAlegria/main.c
@@ -3,6 +3,24 @@
 #include "scanner.h"
 
 int main() {
+
+    const char *nombres_token[] = {
+        "Identificador",       // 100 - 100 = 0
+        "Constante",            // 101 - 100 = 1
+        "Suma",                 // 102
+        "Asignacion +=",        // 103
+        "Resta",                // 104
+        "Asignacion -=",        // 105
+        "Multiplicacion",       // 106
+        "Asignacion *=",        // 107
+        "Division",             // 108
+        "Asignacion /=",        // 109
+        "Potencia",             // 110
+        "Asignacion =",         // 111
+        "Fin de Texto (FDT)",   // 112
+        "Nueva Linea"           // 113
+    };
+
     Token token_actual;
 
     printf("Iniciando escaner. Ingrese expresiones (Ctrl+Z en Windows para FDT/EOF):\n");
@@ -13,56 +31,18 @@ int main() {
         token_actual = (Token)escanear(stdin);
 
         // Se evalúa el token retornado y se imprime según las reglas de la consigna
-        switch (token_actual) {
-            case TOKEN_ID:
-                printf("Token: Identificador\t| Lexema: %s\n", obtener_lexema());
-                break;
-            case TOKEN_CTE:
-                printf("Token: Constante\t| Lexema: %s\n", obtener_lexema());
-                break;
-            case TOKEN_SUMA:
-                printf("Token: Suma\n");
-                break;
-            case TOKEN_MAS_IGUAL:
-                printf("Token: Asignacion += \n");
-                break;
-            case TOKEN_RESTA:
-                printf("Token: Resta\n");
-                break;
-            case TOKEN_MENOS_IGUAL:
-                printf("Token: Asignacion -= \n");
-                break;
-            case TOKEN_MULT:
-                printf("Token: Multiplicacion\n");
-                break;
-            case TOKEN_MULT_IGUAL:
-                printf("Token: Asignacion *= \n");
-                break;
-            case TOKEN_DIV:
-                printf("Token: Division\n");
-                break;
-            case TOKEN_DIV_IGUAL:
-                printf("Token: Asignacion /= \n");
-                break;
-            case TOKEN_POTENCIA:
-                printf("Token: Potencia\n");
-                break;
-            case TOKEN_IGUAL:
-                printf("Token: Asignacion =\n");
-                break;
-            case TOKEN_ERROR_LEXICO:
-                printf("Error lexico\t\t| Lexema: %s\n", obtener_lexema());
-                break;
-            case TOKEN_FDT:
-                printf("Token: Fin de Texto (FDT) reconocido. \n");
-                break;
-            case TOKEN_NL:
-                printf("Token: Nueva Linea (\\n)\n");
-                break;
-            default:
-                printf("Estado desconocido (%d)\n", token_actual);
-                break;
-        }
+        if (token_actual == TOKEN_ERROR_LEXICO) {
+            printf("Error lexico\t\t| Lexema: %s\n", obtener_lexema());
+        } else if (token_actual >= TOKEN_ID && token_actual <= TOKEN_NL) {
+            printf("Token: %s", nombres_token[token_actual - 100]);
+            if (token_actual == TOKEN_ID || token_actual == TOKEN_CTE) {
+                printf("\t| Lexema: %s", obtener_lexema());
+            }
+            printf("\n");
+        } else {
+            printf("Estado desconocido (%d)\n", token_actual);
+         }
+
     } while (token_actual != TOKEN_FDT);
 
     return EXIT_SUCCESS;
