0e4f9391b6c9d9b0c8df76cc41db473cc7e1462a
Alejandro Aguilar Zabala <aaguilarzabala@frba.utn.edu.ar>
2026-09-15 17:06:44 -0300

    Agregado de documentacion
    Se agrego documentación en comentarios a fin de mas claridad

--- TP2 - Version JoaquinAlegria/scanner.h
+++ TP2 - Version JoaquinAlegria/scanner.h
@@ -5,21 +5,24 @@
 
 // Enumerado que mapea los tokens a los estados aceptores y de error de la Tabla de Transición.
 typedef enum {
-    TOKEN_ID = 100,
-    TOKEN_CTE = 101,
-    TOKEN_SUMA = 102,
-    TOKEN_MAS_IGUAL = 103,
-    TOKEN_RESTA = 104,
-    TOKEN_MENOS_IGUAL = 105,
-    TOKEN_MULT = 106,
-    TOKEN_MULT_IGUAL = 107,
-    TOKEN_DIV = 108,
-    TOKEN_DIV_IGUAL = 109,
-    TOKEN_POTENCIA = 110,
-    TOKEN_IGUAL = 111,
-    TOKEN_FDT = 112,
-    TOKEN_NL = 113,
-    TOKEN_ERROR_LEXICO = 200
+    // Aceptores (>= 100)
+    TOKEN_ID          = 100,  // Identificador: letra seguida de letras/dígitos
+    TOKEN_CTE         = 101,  // Constante: entero o real (double)
+    TOKEN_SUMA        = 102,  // '+'
+    TOKEN_MAS_IGUAL   = 103,  // '+='
+    TOKEN_RESTA       = 104,  // '-'
+    TOKEN_MENOS_IGUAL = 105,  // '-='
+    TOKEN_MULT        = 106,  // '*'
+    TOKEN_MULT_IGUAL  = 107,  // '*='
+    TOKEN_DIV         = 108,  // '/'
+    TOKEN_DIV_IGUAL   = 109,  // '/='
+    TOKEN_POTENCIA    = 110,  // '^'
+    TOKEN_IGUAL       = 111,  // '='
+    TOKEN_FDT         = 112,  // Fin de texto (EOF)
+    TOKEN_NL          = 113,  // Nueva línea (para el prompt del intérprete)
+
+    // Errores (>= 200)
+    TOKEN_ERROR_LEXICO = 200  // Error lexico, agrupa fallos
 } Token;
 
 // Declaración de la función principal del escáner.
